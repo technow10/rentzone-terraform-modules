44c84143797154aca58374e5db314023dd7537ca
Techaddict <kindheart9ja@gmail.com>
2024-04-17 12:57:06 -0500

    update

--- security-group/variable.tf
+++ security-group/variable.tf
@@ -1,9 +1,5 @@
-# Create variables for security groups
+# Create variables for security groups/ do not add all the resources created
 variable "project_name" {}
 variable "environment" {}
 variable "vpc_id" {}
-variable "alb_security_group" {}
-variable "bastion_security_group" {}
-variable "app_server_security_group" {}
-variable "database_security_group" {}
 variable "ssh-ip" {}
