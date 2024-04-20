4b03aedeef38ceed89aa85255f2050968ffdd8cd
Techaddict <kindheart9ja@gmail.com>
2024-04-20 16:18:23 -0500

    update

--- rds/variable.tf
+++ rds/variable.tf
@@ -8,7 +8,5 @@ variable "database_instance_class" {}
 variable "availability_zone_1" {}
 variable "db_instance_identifier" {}
 variable "multi_az_deployment" {}
-variable "database_security_group_id" {
-  
-}
+variable "database_security_group_id" {}
 
