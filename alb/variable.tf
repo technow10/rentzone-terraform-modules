c630af9e817aa1405ac7602a3e04f7848bdba4c4
Techaddict <kindheart9ja@gmail.com>
2024-05-01 08:13:52 -0500

    Created ALB

---
+++ alb/variable.tf
@@ -0,0 +1,8 @@
+variable "project_name" {}
+variable "environment" {}
+variable "alb_security_group_id" {}
+variable "private_data_subnet_az1_id" {}
+variable "private_data_subnet_az2_id" {}
+variable "target_type" {}
+variable "vpc_id" {}
+variable "certificate_arn" {}
